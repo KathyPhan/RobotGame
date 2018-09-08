c484d9bb30baf2b39472df00b70fe36dbbb1b833
Kathleen Phan <katphan96@gmail.com>
2018-09-08 12:23:06 -0700

    New Commit

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu");
+        //Debug.Log("Quit");
+        //Application.Quit();
+    }
+}
